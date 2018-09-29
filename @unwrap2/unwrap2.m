1defcf1262c24e0d107eb95c8b9087469b3b297e
marcsous <17042435+marcsous@users.noreply.github.com>
2018-09-29 10:07:18 -0700

    Add files via upload

--- @unwrap2/unwrap2.m
+++ @unwrap2/unwrap2.m
@@ -13,5 +13,11 @@ function output = unwrap2(varargin)
 %Outputs
 %-output: unwrapped phase
 
+mexbin = [mfilename('fullpath') '.' mexext];
+
+if ~exist(mexbin,'file')
+    mexcpp = [mfilename('fullpath') '.cpp'];
+    mex('-v',mexcpp,'-output',mexbin);
+end
 
 output = unwrap2(varargin{:});
